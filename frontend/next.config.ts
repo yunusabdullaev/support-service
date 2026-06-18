d43950c91281f1b3e7923d97707e51c39af31844
Yunus <151526757+yunusabdullaev@users.noreply.github.com>
2026-06-18 12:42:12 +0500

    fix: disable Vercel toolbar

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  devIndicators: false,
+  vercelToolbar: { enabled: false },
 };
 
 export default nextConfig;
